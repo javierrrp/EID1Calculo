aff6bf96005a905ac2ee9ed971a6d3b21760142c
javierrrp <javier.poblete2023@alu.uct.cl>
2026-05-10 22:24:07 -0400

    clasificacion de conicas (beta)

--- controllers/controlador_conicas.py
+++ controllers/controlador_conicas.py
@@ -1,5 +1,35 @@
 from view.vista_conicas import VistaConicas
+from models.modelo_conicas import ModeloConicas
+from controllers.controlador_rut import ControladorRut
 
 class ControladorConicas:
-    def __init__(self):
+    def __init__(self, controlador_rut):
         self.vista = VistaConicas()
+        self.controlador_rut = controlador_rut
+
+
+    def ejecutar_modulo(self):
+        
+        datos = self.controlador_rut.datos_ecuacion
+
+        if datos is None:
+            self.vista.lbl_titulo.setText("Ingrese un RUT válido en la pestaña RUT para analizar la cónica.")
+            return
+
+        A = datos["A"]; B= datos["B"]; C = datos["C"]; D = datos["D"]; E = datos["E"]
+
+        self.modelo_conicas = ModeloConicas(A, B, C, D, E)
+
+        tipo = self.modelo_conicas.clasificar_conica()
+
+        h, k, lado_der = self.modelo_conicas.completar_cuadrados()
+
+        self.vista.lbl_titulo.setText(f"Resultado: {tipo}")
+        self.vista.txt_procedimiento.setText(self.modelo_conicas.obtener_pasos_texto())
+        self.vista.input_centro.setText(f"({h}, {k})")
+    
+        if tipo == "Circunferencia":
+            # Si es circunferencia, el lado derecho es el Radio al cuadrado
+            self.vista.input_focos.setText(f"Radio al cuadrado = {lado_der}")
+        else:
+            self.vista.input_focos.setText("Focos por calcular...")
--- view/vista_conicas.py
+++ view/vista_conicas.py
@@ -1,7 +1,28 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import *
 
 class VistaConicas(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Módulo de Cónicas")
+        self.setMinimumSize(400, 500)
+
         layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Módulo de Secciones Cónicas Listo"))
+
+        self.lbl_titulo = QLabel("Analizando Conica...")
+        layout.addWidget(self.lbl_titulo)
+
+        layout.addWidget(QLabel("Desarrollo matematico:"))
+        self.txt_procedimiento = QTextEdit()
+        self.txt_procedimiento.setReadOnly(True)
+        layout.addWidget(self.txt_procedimiento)
+
+
+        layout.addWidget(QLabel("Elementos geometricos:"))
+
+        layout.addWidget(QLabel("Centro (h, k):"))
+        self.input_centro = QLineEdit()
+        layout.addWidget(self.input_centro)
+
+        layout.addWidget(QLabel("Focos / Vertices:"))
+        self.input_focos = QLineEdit()
+        layout.addWidget(self.input_focos)
